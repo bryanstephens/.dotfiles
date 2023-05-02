bd0612d555da304479f232ebcfe03f2eb263cdff
Bryan Stephens <bryan.craig.stephens@gmail.com>
2023-05-02 01:27:47 -0700

    Change telescope to live_grep and allow searching hidden directories

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -3,11 +3,17 @@ if not ok then
   return
 end
 
+require("telescope").setup({
+  pickers = {
+    live_grep = {
+      additional_args = {"--hidden"}
+    }
+  }
+})
+
 vim.keymap.set('n', '<leader>pf', function()
   builtin.find_files({hidden = true})
 end, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
+vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fs', builtin.treesitter, {})
