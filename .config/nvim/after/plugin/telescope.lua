2177508dc2ccbcc3aaf337d52c370d8f497f385c
Bryan Stephens <bryan.craig.stephens@gmail.com>
2023-05-02 01:17:20 -0700

    Fix telescope not finding hidden files and directories

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -3,7 +3,9 @@ if not ok then
   return
 end
 
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>pf', function()
+  builtin.find_files({hidden = true})
+end, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
