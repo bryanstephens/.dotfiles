331b6edf06621ccc38e514d44fc121a9c8201da4
Bryan Stephens <bryan.stephens@infusionsoft.com>
2023-05-12 12:39:51 -0700

    Switch from tmux to zellij

--- install.sh
+++ install.sh
@@ -10,20 +10,16 @@ if [ $? -gt 0 ]; then
 fi
 
 brew install --cask font-awesome-terminal-fonts
-brew install tmux neovim ripgrep stow asdf gpg gawk
+brew install neovim ripgrep stow asdf gpg gawk
 
 stow . -d ${SCRIPT_DIR} -t ${HOME} -v
 
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf plugin add python
 asdf plugin-add direnv
+asdf plugin-add zellij
 asdf install
 
-# Install TPM (Tmux plugin manager) if it doesn't exist and all configured plugins
-if [ ! -d $HOME/.tmux/plugins/tpm ]; then
-  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-  ~/.tmux/plugins/tpm/bin/install_plugins
-fi
 
 if [ ! -d $HOME/.local/share/nvim/site/pack/packer/start/packer.nvim ]; then
   git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
