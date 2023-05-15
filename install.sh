796663aadad2dc9775afc61fc7148254359d3087
Bryan Stephens <bryan.craig.stephens@gmail.com>
2023-05-14 23:31:42 -0700

    Resync neovim plugins every time install script is run

--- install.sh
+++ install.sh
@@ -27,5 +27,6 @@ asdf direnv setup --shell zsh --version 2.32.2
 
 if [ ! -d $HOME/.local/share/nvim/site/pack/packer/start/packer.nvim ]; then
   git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-  nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 fi
+
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
