3cb2e577df9dce354cba3cfac37e9d28ab60afe4
Bryan Stephens <bryan.stephens@infusionsoft.com>
2023-12-14 13:37:12 -0700

    Fix small harpoon popover width

--- .config/nvim/lua/bryanstephens/plugins/harpoon.lua
+++ .config/nvim/lua/bryanstephens/plugins/harpoon.lua
@@ -4,6 +4,11 @@ return {
     'nvim-lua/plenary.nvim'
   },
   config = function()
+    require('harpoon').setup({
+      menu = {
+        width = vim.api.nvim_win_get_width(0) - 4,
+      }
+    })
     local mark = require('harpoon.mark')
     local ui = require('harpoon.ui')
 
