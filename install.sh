475cc80c44fa83657938c477ce872c88dd205324
Bryan Stephens <bryan.stephens@infusionsoft.com>
2024-10-10 07:35:09 -1000

    Update asdf plugin install syntax

--- install.sh
+++ install.sh
@@ -19,11 +19,12 @@ asdf plugin add gcloud
 asdf plugin add java
 asdf plugin add maven
 asdf plugin add python
-asdf plugin-add direnv
-asdf plugin-add zellij
-asdf plugin-add rust
-asdf plugin-add golang
-asdf plugin-add jq
+asdf plugin add direnv
+asdf plugin add zellij
+asdf plugin add rust
+asdf plugin add golang
+asdf plugin add terraform https://github.com/asdf-community/asdf-hashicorp.git
+asdf plugin add jq
 asdf install
 
 asdf direnv setup --shell zsh --version 2.32.2
